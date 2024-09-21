b667e315b46030b389efb23a65f69a8b0c7c5449
CesarTun <ctuncetina@gmail.com>
2024-09-20 23:39:21 -0600

    feat: add (FFFBusiness) contract to project and modify deploy file

---
+++ packages/hardhat/deploy/00_deploy_FFFBusiness.ts
@@ -0,0 +1,22 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { Contract } from "ethers";
+
+const deployFFFBusiness: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  await deploy("FFFBusiness", {
+    from: deployer,
+    args: [],
+    log: true,
+    autoMine: true,
+  });
+
+  const newContract = await hre.ethers.getContract<Contract>("FFFBusiness", deployer);
+  console.log("🏆 Friends and Family Funds is available with address:", await newContract.getAddress());
+};
+
+export default deployFFFBusiness;
+
+deployFFFBusiness.tags = ["FFFBusiness"];
