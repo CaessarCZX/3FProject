0bc7a7eff7ce8347b91fd5506d8262a3d1f2b9a7
CesarTun <ctuncetina@gmail.com>
2024-09-25 22:04:13 -0600

    feat: add dployment files and ProtectedRoutes component

---
+++ packages/nextjs/services/Auth/ProtectedRoute.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ComponentType, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAccount } from "wagmi";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
+
+export default function ProtectedRoute(Outlet: ComponentType): ComponentType {
+  return function ProtectedRoute(props: any) {
+    const router = useRouter();
+    const currentMember = useAccount();
+
+    const { data: isActiveMember } = useScaffoldReadContract({
+      contractName: "FFFBusiness",
+      functionName: "isCurrentlyActiveUser",
+      args: [currentMember?.address],
+    });
+    useEffect(() => {
+      if (isActiveMember == false || isActiveMember == undefined) router.push("/");
+      console.log(isActiveMember);
+    }, []);
+
+    if (isActiveMember == undefined) return null;
+
+    return <Outlet {...props} />;
+  };
+}
