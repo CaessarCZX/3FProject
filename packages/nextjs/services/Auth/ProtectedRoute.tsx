dfcb27897735c88eae1ec86b12abb84299c29e50
CesarTun <ctuncetina@gmail.com>
2024-11-30 11:00:03 -0600

    feat: update and uncomment (ProtectedRoutes) component

--- packages/nextjs/services/Auth/ProtectedRoute.tsx
+++ packages/nextjs/services/Auth/ProtectedRoute.tsx
@@ -1,27 +1,35 @@
 "use client";
 
-import { ComponentType } from "react";
-
-// import { useRouter } from "next/navigation";
-// import { useAccount } from "wagmi";
-// import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
+import { ComponentType, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAccount } from "wagmi";
+import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
 
 export default function ProtectedRoute(Outlet: ComponentType): ComponentType {
   return function ProtectedRoute(props: any) {
-    // const router = useRouter();
-    // const currentMember = useAccount();
+    const router = useRouter();
+    const currentMember = useAccount();
 
-    // const { data: isActiveMember } = useScaffoldReadContract({
-    //   contractName: "FFFBusiness",
-    //   functionName: "isCurrentlyActiveUser",
-    //   args: [currentMember?.address],
-    // });
-    // useEffect(() => {
-    //   if (isActiveMember == false || isActiveMember == undefined) router.push("/");
-    //   console.log(isActiveMember);
-    // }, []);
+    // Recupera el status del la wallet del usuario en el contrato
+    const { data: isActiveMember } = useScaffoldReadContract({
+      contractName: "FFFBusiness",
+      functionName: "checkActiveMember",
+      args: [currentMember?.address],
+    });
 
-    // if (isActiveMember == undefined) return null;
+    // Se activa una sola vez para por cada renderizacion de los componentes protegidos
+    // Solo funciona con un solo componente (Dashboard por el momento)
+    useEffect(() => {
+      if (isActiveMember == false || isActiveMember == undefined) {
+        setTimeout(() => {
+          router.push("/");
+          console.log(isActiveMember);
+        }, 500);
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    if (isActiveMember == undefined) return null;
 
     return <Outlet {...props} />;
   };
