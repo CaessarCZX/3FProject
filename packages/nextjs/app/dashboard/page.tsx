bbf2d730ccd0b370793529120546bb663c9236e1
CesarTun <ctuncetina@gmail.com>
2024-12-01 11:40:24 -0600

    feat: add (BlockExplorer, TransactionTable) component to dashboard

--- packages/nextjs/app/dashboard/page.tsx
+++ packages/nextjs/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import BlockExplorer from "./_components/BlockExplorer";
 import DepositContract from "./_components/DepositContract";
 import ContentStats from "./_content/ContentStats";
 import { NextPage } from "next";
@@ -17,6 +18,12 @@ const Dashboard: NextPage = () => {
               </CardDataStats>
             );
           })}
+        </div>
+        <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+          <div className="col-span-12">
+            {/* Add: xl:col-span-8 to className if neccesary */}
+            <BlockExplorer />
+          </div>
         </div>
       </InternalLayout>
     </>
