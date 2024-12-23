00ad591217a0d30fb31700fff4abd04c9d6ee643
CesarTun <ctuncetina@gmail.com>
2024-12-23 13:54:43 -0600

    chore: add internal layout to expansion page

--- packages/nextjs/app/expansion/page.tsx
+++ packages/nextjs/app/expansion/page.tsx
@@ -1,10 +1,11 @@
 import { NextPage } from "next";
+import InternalLayout from "~~/components/Layouts/InternalLayout";
 
 const Expansion: NextPage = () => {
   return (
-    <>
+    <InternalLayout>
       <p>Expansion :V</p>
-    </>
+    </InternalLayout>
   );
 };
 
