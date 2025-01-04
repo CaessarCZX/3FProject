71b57c55c3b23dd9fe38af9a422c5d0bacb4c085
Alexisdlr1 <alexis@steamhub.com.mx>
2025-01-03 21:32:00 -0700

    new validate change password

--- packages/nextjs/app/resetPassword/_components/resetPassword.tsx
+++ packages/nextjs/app/resetPassword/_components/resetPassword.tsx
@@ -1,38 +1,35 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { FiLock } from "react-icons/fi";
 import { RiEyeCloseLine, RiEyeLine } from "react-icons/ri";
-import { useAccount } from "wagmi";
-import { WalletConnectionBtn } from "~~/components/Wallet/WalletConectionBtn";
 import { notification } from "~~/utils/scaffold-eth/notification";
 
 export const ResetPassword = () => {
   // State management
   const [showpass, setShowpass] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
-  const [formData, setFormData] = useState({ email: "", newPassword: "", wallet: "" });
+  const [formData, setFormData] = useState({ email: "", newPassword: "", token: "" });
   const [errorMessage, setErrorMessage] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [isWalletConnected, setIsWalletConnected] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
   const router = useRouter();
-  const currentUser = useAccount();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
-  // Pre-fill the email from URL params
+  // Pre-fill the email and token from URL params
   useEffect(() => {
     const currentParams = new URL(window.location.href, window.location.origin);
     const email = currentParams.searchParams.get("email") ?? "";
+    const token = currentParams.searchParams.get("token") ?? "";
 
-    if (email && !formData.email) {
-      setFormData(prevData => ({ ...prevData, email }));
+    if (email && token) {
+      setFormData(prevData => ({ ...prevData, email, token }));
     }
-  }, [formData]);
+  }, []);
 
   useEffect(() => {
     if (errorMessage) {
@@ -46,69 +43,16 @@ export const ResetPassword = () => {
     }
   }, [errorMessage, successMessage]);
 
-  const handleConnectWallet = useCallback(async () => {
-    setErrorMessage("");
-    setSuccessMessage("");
-
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_BACKEND}/f3api/users/check-wallet`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ wallet: formData.wallet }),
-        credentials: "include",
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-
-        if (data.exists) {
-          setSuccessMessage("Wallet conectada con éxito.");
-          setIsWalletConnected(true);
-        } else {
-          setErrorMessage("Esta wallet no está registrada.");
-          setIsWalletConnected(false);
-        }
-      } else {
-        const errorData = await response.json();
-        setErrorMessage(errorData.message || "Error al verificar la wallet.");
-        setIsWalletConnected(false);
-      }
-    } catch (error) {
-      setErrorMessage("No se pudo conectar con el servidor.");
-      setIsWalletConnected(false);
-    }
-  }, [formData.wallet]);
-
-  useEffect(() => {
-    if (currentUser.status === "connected") {
-      setFormData(prevData => ({ ...prevData, wallet: currentUser.address ?? "" }));
-      const checkConnect = setTimeout(() => handleConnectWallet(), 2000);
-      return () => clearTimeout(checkConnect);
-    }
-
-    if (currentUser.status === "disconnected") {
-      setFormData(prevData => ({ ...prevData, wallet: "" }));
-    }
-  }, [currentUser.status, currentUser.address, handleConnectWallet]);
-
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setIsSubmitting(true);
     setErrorMessage("");
 
-    if (!isWalletConnected) {
-      setErrorMessage("No tienes conectada una wallet válida.");
-      setIsSubmitting(false);
-      return;
-    }
-
     try {
       const response = await fetch(`${process.env.NEXT_PUBLIC_API_BACKEND}/f3api/users/resetPassword`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(formData), // Incluye el token y email en la solicitud
       });
 
       if (response.ok) {
@@ -141,7 +85,6 @@ export const ResetPassword = () => {
             id="email"
             autoComplete="username"
             value={formData.email}
-            readOnly
             className="block w-full pr-10 pl-4 py-2 border border-gray-300 rounded-md shadow-sm bg-gray-100 focus:outline-none sm:text-sm"
             required
           />
@@ -155,7 +98,7 @@ export const ResetPassword = () => {
         </label>
         <div className="mt-1 relative">
           <input
-            type={showpass ? "type" : "password"}
+            type={showpass ? "text" : "password"}
             id="newPassword"
             value={formData.newPassword}
             onChange={handleChange}
@@ -183,22 +126,12 @@ export const ResetPassword = () => {
         </div>
       </div>
 
-      {/* Wallet connection */}
-      <div>
-        <label htmlFor="wallet" className="block text-sm font-medium text-gray-700">
-          Wallet
-        </label>
-        <div className="mt-1 relative flex">
-          <WalletConnectionBtn enableWallet={isWalletConnected} classBtn="w-full rounded-md" />
-        </div>
-      </div>
-
       {/* Submit */}
       <button
         type="submit"
-        disabled={isSubmitting || !isWalletConnected}
+        disabled={isSubmitting}
         className={`w-full py-2 px-4 border border-transparent disabled:bg-gray-300 rounded-md shadow-sm text-sm font-medium text-white ${
-          isSubmitting || !isWalletConnected ? "bg-gray-500" : "bg-gray-900 hover:bg-gray-700"
+          isSubmitting ? "bg-gray-500" : "bg-gray-900 hover:bg-gray-700"
         }`}
       >
         {isSubmitting ? "Actualizando contraseña..." : "Actualizar contraseña"}
