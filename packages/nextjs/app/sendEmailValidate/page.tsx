e7800e75ae38f7caae21d37c21bb43b61422cc65
Alexisdlr1 <alexis@steamhub.com.mx>
2025-01-05 13:21:02 -0700

    new design reset password

--- packages/nextjs/app/sendEmailValidate/page.tsx
+++ packages/nextjs/app/sendEmailValidate/page.tsx
@@ -4,23 +4,27 @@ import withAccessControl from "../hoc/withAccessControl";
 import { SendEmailValidate } from "./_components/sendEmailValidate";
 import type { NextPage } from "next";
 
-// import { SignInForm } from "~~/app/login/_components/SignInFrom";
-
 const validateEmail: NextPage = () => {
   return (
-    <div className="min-h-screen flex">
-      {/* Left Section */}
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* Imagen de fondo para móviles y columna izquierda para pantallas grandes */}
       <div
-        className="w-1/2 bg-cover bg-center"
+        className="hidden md:block md:w-1/2 bg-cover bg-center"
         style={{
           backgroundImage: "url(https://cdn.easyfrontend.com/pictures/contact/contact13.jpg)",
         }}
       ></div>
 
-      {/* Right Section */}
-      <div className="w-1/2 flex items-center justify-center bg-white">
-        <div className="max-w-md w-full space-y-8">
-          <h2 className="text-3xl font-bold text-gray-900 text-center">Cambiar Contraseña</h2>
+      <div
+        className="md:hidden bg-cover bg-center absolute inset-0 -z-10"
+        style={{
+          backgroundImage: "url(https://cdn.easyfrontend.com/pictures/contact/contact13.jpg)",
+        }}
+      ></div>
+
+      {/* Sección de contenido */}
+      <div className="w-full md:w-1/2 flex items-center justify-center bg-white bg-opacity-80 md:bg-opacity-100 min-h-screen md:min-h-0">
+        <div className="max-w-md w-full space-y-8 p-6 md:p-0">
           <SendEmailValidate />
         </div>
       </div>
