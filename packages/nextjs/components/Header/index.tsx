40311c667abb93884878bb958c34deab1960a396
CesarTun <ctuncetina@gmail.com>
2025-01-24 11:56:24 -0600

    feat: add premium member medal for header

--- packages/nextjs/components/Header/index.tsx
+++ packages/nextjs/components/Header/index.tsx
@@ -1,62 +1,79 @@
+import Image from "next/image";
 import { SwitchTheme } from "../Actions/Theme/SwitchTheme";
+import DropdownUser from "./DropdownUser";
+import { useGetMemberStatus } from "~~/hooks/user/useGetMemberStatus";
+
 // import DropdownMessage from "./DropdownMessage";
 // import DropdownNotification from "./DropdownNotification";
 // import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import DropdownUser from "./DropdownUser";
 
 const Header = (props: { sidebarOpen: string | boolean | undefined; setSidebarOpen: (arg0: boolean) => void }) => {
+  const { memberStatus } = useGetMemberStatus();
   return (
     <header className="sticky top-0 z-40 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-      <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 lg:justify-end 2xl:px-11">
-        <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
-          {/* <!-- Hamburger Toggle BTN --> */}
-          <button
-            aria-controls="sidebar"
-            onClick={e => {
-              e.stopPropagation();
-              props.setSidebarOpen(!props.sidebarOpen);
-            }}
-            className="z-50 block rounded-sm border border-stroke bg-white p-2 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
-          >
-            <span className="relative block h-6 w-6 cursor-pointer">
-              <span className="du-block absolute right-0 h-full w-full">
-                <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!w-full delay-300"
-                  }`}
-                ></span>
-                <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "delay-400 !w-full"
-                  }`}
-                ></span>
-                <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!w-full delay-500"
-                  }`}
-                ></span>
+      <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+        {/* <!-- Items for lesft side --> */}
+        <div className="flex">
+          <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
+            {/* <!-- Hamburger Toggle BTN --> */}
+            <button
+              aria-controls="sidebar"
+              onClick={e => {
+                e.stopPropagation();
+                props.setSidebarOpen(!props.sidebarOpen);
+              }}
+              className="z-50 block rounded-sm border border-stroke bg-white p-2 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
+            >
+              <span className="relative block h-6 w-6 cursor-pointer">
+                <span className="du-block absolute right-0 h-full w-full">
+                  <span
+                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
+                      !props.sidebarOpen && "!w-full delay-300"
+                    }`}
+                  ></span>
+                  <span
+                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
+                      !props.sidebarOpen && "delay-400 !w-full"
+                    }`}
+                  ></span>
+                  <span
+                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
+                      !props.sidebarOpen && "!w-full delay-500"
+                    }`}
+                  ></span>
+                </span>
+                <span className="absolute right-0 h-full w-full rotate-45">
+                  <span
+                    className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
+                      !props.sidebarOpen && "!h-0 !delay-[0]"
+                    }`}
+                  ></span>
+                  <span
+                    className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
+                      !props.sidebarOpen && "!h-0 !delay-200"
+                    }`}
+                  ></span>
+                </span>
               </span>
-              <span className="absolute right-0 h-full w-full rotate-45">
-                <span
-                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!h-0 !delay-[0]"
-                  }`}
-                ></span>
-                <span
-                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!h-0 !delay-200"
-                  }`}
-                ></span>
-              </span>
-            </span>
-          </button>
-          {/* <!-- Hamburger Toggle BTN --> */}
+            </button>
+            {/* <!-- Hamburger Toggle BTN --> */}
+          </div>
 
-          {/* <Link className="block flex-shrink-0 lg:hidden" href="/">
-            <Image width={32} height={32} src={"/logo/logo_black.svg"} alt="Logo" />
-          </Link> */}
+          {/* Premium user distinction */}
+          {memberStatus && (
+            <div className="ml-2 lg:ml-0 flex">
+              <Image width={40} height={80} src={"/premium/gold-medal.svg"} alt="usuario premium" />
+              <div className="text-brand-default">
+                <h3 className="font-bold m-0">MIEMBRO</h3>
+                <h3 className="font-bold m-0">VIP</h3>
+              </div>
+            </div>
+          )}
+          {/* Premium user distinction */}
         </div>
+        {/* <!-- Items for lesft side --> */}
 
+        {/* Search bar */}
         {/* <div className="hidden sm:block">
           <form action="https://formbold.com/s/unique_form_id" method="POST">
             <div className="relative">
@@ -72,6 +89,7 @@ const Header = (props: { sidebarOpen: string | boolean | undefined; setSidebarOp
             </div>
           </form>
         </div> */}
+        {/* Search bar */}
 
         <div className="flex items-center gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
